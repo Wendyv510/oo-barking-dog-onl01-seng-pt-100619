4c4e8029797be55e9cb28eeff2718e678468c3cf
Wendy Harris <wendyv510@hotmail.com>
2019-10-22 18:57:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,13 @@ class Dog
     puts "Woof!"
   end
   
-  def name= dogs_name
+  def name= (dogs_name)
+    @this_dogs_name = dogs_name
     
+  end 
+  
+  def name
+    @this_dogs_name 
   end 
   
 end 
